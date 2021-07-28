86c39ef425a2e4b236db7354fb851e2005158b13
Agastyav9 <73377219+Agastyav9@users.noreply.github.com>
2021-07-28 18:33:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup()
+    {
+        canvas = createCanvas(300, 300);
+        canvas.center();
+        video = createCapture(VIDEO);
+        video.size(300, 300);
+        video.hide();
+
+    }
+
+    console.log('ml5.version',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/yqu7052Qa/model.json',modelLoaded);
+function modelLoaded()
+{
+  console.log("model is loaded");
+}
+
+
+    
